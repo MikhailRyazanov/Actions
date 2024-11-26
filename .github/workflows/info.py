a46fe418d94ceb447fa6e8dbe1a682bc2b5cda8b
MikhailRyazanov <mikhail.ryazanov@gmail.com>
2024-11-25 20:26:21 -0700

    Info, unindent

---
+++ .github/workflows/info.py
@@ -0,0 +1,19 @@
+import platform, sys, os
+
+print('Platorm:', platform.platform())
+print('Python:', sys.version)
+
+try:
+    import numpy
+    print('NumPy:', numpy.version.full_version)
+except ImportError:
+    print('NumPy not found')
+
+try:
+    import scipy
+    print('SciPy:', scipy.version.full_version)
+except ImportError:
+    print('SciPy not found')
+
+sys.stdout.flush()
+os.system('cython -V 2>cython.err') and print('Cython not found')
