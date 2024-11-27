28048b96bf56cee3c697cf4860f26f21398747f7
MikhailRyazanov <mikhail.ryazanov@gmail.com>
2024-11-26 21:31:37 -0700

    Markdown summary

--- .github/workflows/summary.py
+++ .github/workflows/summary.py
@@ -1,17 +1,40 @@
 """
 Print summary of all pytest results (saved using "--junit-xml=<wheel>.xml").
+Without arguments -- ANSI-colored terminal, with any argument -- markdown.
 """
+import sys
 from glob import glob
 from re import findall
 
-# colors
-g = lambda v: '' if v else '\x1B[32m'  # green if 0
-r = lambda v: '\x1B[31m' if v else ''  # red if not 0
-y = lambda v: '\x1B[33m' if v else ''  # yellow if not 0
-_ = '\x1B[0m'  # reset
+# markdown or terminal
+md = len(sys.argv) > 1  # (any argument)
 
-print('Wheel  Pass  Error  Fail  Skip')
-print('==============================')
+
+def fmt(val, cond, style):  # style: "ok"/"warn"/"err"
+    if not cond:
+        return val
+    if md:
+        val = val.strip()
+        if style in ['ok', 'err']:
+            return '**' + val + '**'  # bold
+        else:
+            return '*' + val + '*'  # italic
+    if style == 'ok':
+        return f'\x1B[32m{val}\x1B[0m'  # green
+    elif style == 'err':
+        return f'\x1B[31m{val}\x1B[0m'  # red
+    else:
+        return f'\x1B[33m{val}\x1B[0m'  # yellow
+
+
+if md:
+    print('| Wheel | Pass | Error | Fail | Skip |')
+    print('| --- | --- | --- | --- | --- |')
+    sep = ' | '
+else:
+    print('Wheel  Pass  Error  Fail  Skip')
+    print('==============================')
+    sep = '  '
 
 good, bad = 0, 0
 
@@ -23,18 +46,26 @@ for xml in sorted(glob('*.xml')):
     P = T - E - F - S
     if xml[:5] == 'sdist':
         T -= S  # ignore skipped Cython
-        s = y
+        s = 'warn'
     else:
-        s = r
-    print(f'{xml[:-4]}\n{"":5}'
-          f'  {g(T-P)}{P:4}{_}'
-          f'  {r(E)}{E:5}{_}'
-          f'  {r(F)}{F:4}{_}'
-          f'  {s(S)}{S:4}{_}')
+        s = 'err'
+    row = sep.lstrip()
+    row += xml[:-4]
+    if not md:
+        row += '\n' + ' ' * 5
+    row += sep + fmt(f'{P:4}', P == T, 'ok')
+    row += sep + fmt(f'{E:5}', E, 'err')
+    row += sep + fmt(f'{F:4}', F, 'err')
+    row += sep + fmt(f'{S:4}', S, s)
+    row += sep.rstrip()
+    print(row)
     if P == T:
         good += 1
     else:
         bad += 1
 
-print('==============================')
-print(f'{g(bad)}{good} good{_}{f", {r(bad)}{bad} bad{_}" if bad else ""}')
+print('' if md else '==============================')
+tot = fmt(f'{good} good', not bad, 'ok')
+if bad:
+    tot += ', ' + fmt(f'{bad} bad', True, 'err')
+print(tot)
