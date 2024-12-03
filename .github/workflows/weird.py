cb41b688ce2a3a9121bd5461f3d3d0971140f4d2
MikhailRyazanov <mikhail.ryazanov@gmail.com>
2024-12-02 20:30:01 -0700

    test legendre

--- .github/workflows/weird.py
+++ .github/workflows/weird.py
@@ -1,4 +1,17 @@
 import numpy as np
 from scipy.special import legendre
 
-print('Test!')
+print('Test legendre:')
+
+def ang_legendre(c):
+    print(f'{c=}')
+    C = np.zeros_like(c, dtype=float)
+    for n, a in enumerate(c):
+        print(f'{n=}: {a} *', legendre(n).c[::2])
+        C[n::-2] += a * legendre(n).c[::2]
+        # (SciPy's legendre() has backwards order and produces noise in
+        #  coefficients that must be zero, so indexing takes care of this)
+    print(f'{C=}')
+    return C
+
+ang_legendre([1, 0, +2])
