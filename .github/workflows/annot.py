aed6f30b6032d1cdcc838068e8da0842c9b59f4d
MikhailRyazanov <mikhail.ryazanov@gmail.com>
2024-12-05 20:48:34 -0700

    Test GitHub messages

---
+++ .github/workflows/annot.py
@@ -0,0 +1,2 @@
+print('::notice title=Test title::Test notice from python')
+print('::warning::Test warning from python')
--- .github/workflows/weird.py
+++
@@ -1,9 +0,0 @@
-from warnings import simplefilter
-simplefilter('always')
-
-from scipy.optimize import curve_fit
-
-print(curve_fit(lambda x, a: a, [0.0, 1.0], [-1.0, 1.0]))
-print(curve_fit(lambda x, a: a + 1, [0.0, 1.0], [-1.0, 1.0]))
-print(curve_fit(lambda x, a: a + 1, [0.0, 1.0], [0.0, 2.0]))
-print(curve_fit(lambda x, a: a, [0.0, 1.0], [-1.0, 1.0], method='trf'))
